c4c199b5c826851fb2d57da8c54c96c562b5897b
JUO-CHOI <csu970902@gmail.com>
2023-11-21 17:54:34 +0900

    231120 문제풀이

---
+++ practice_231120/src/q2.java
@@ -0,0 +1,16 @@
+// Q. 두개의 숫자를 입력받고, 순서를 바꿔서 출력하시오.
+
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(b + " " + a);
+
+    }
+}
