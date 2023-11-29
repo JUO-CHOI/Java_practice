2ea6ee30cc3d709dacb4d4dd10c652c342857190
JUO-CHOI <csu970902@gmail.com>
2023-11-29 18:15:25 +0900

    정리

--- hello_java/src/d6/Ship.java
+++
@@ -1,8 +0,0 @@
-package d6;
-
-public class Ship implements Horn {
-    @Override
-    public void beep() {
-        System.out.println("바아앙");
-    }
-}
--- hello_java/src/d6prob/Electronics.java
+++
@@ -1,8 +0,0 @@
-package d6prob;
-
-public interface Electronics {
-    boolean Switchable(boolean isSwitch);
-    boolean PlaySount(boolean isPlaying);
-    boolean Display(boolean isOn);
-    boolean CookingUtensil(boolean isCooking);
-}
